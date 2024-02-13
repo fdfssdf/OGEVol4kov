d205567aacbea2b2ee941b7d62bfdbd8cf8ec361
fdfssdf <92631907+fdfssdf@users.noreply.github.com>
2024-02-13 20:14:53 +0300

    Add files via upload

---
+++ шпоры.py
@@ -0,0 +1,31 @@
+s1="1 байт =2 битам. => биты/2=байты. байты-2(пробел и запятая)=букв"
+s2
+s3
+s4
+s5
+s6
+s7
+s8="!используй круги эйлера!"
+s9
+s10
+v=int(input("шпору к какому номеру надо? "))
+if v=1:
+    print(s1)
+if v=2:
+    print(s2)
+if v=3:
+    print(s3)
+if v=4:
+    print(s4)
+if v=5:
+    print(s5)
+if v=6:
+    print(s6)
+if v=7:
+    print(s7)
+if v=8:
+    print(s8)
+if v=9:
+    print(s9)
+if v=10:
+    print(s10)
